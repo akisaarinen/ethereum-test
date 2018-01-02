aa14d1930e832031e459eada1e06e28992e5437b
Aki Saarinen <aki.saarinen@reaktor.co.jp>
2018-01-02 23:21:47 +0900

    Add a simple MetaCoin implementation for testing

---
+++ contracts/MetaCoin.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+contract MetaCoin {
+    mapping (address => uint) balances;
+
+    function MetaCoin() public {
+        balances[tx.origin] = 10000;
+    }
+
+    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+        if (balances[msg.sender] < amount)
+            return false;
+
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        return true;
+    }
+
+    function getBalance(address addr) public view returns(uint) {
+        return balances[addr];
+    }
+}
